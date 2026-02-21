f91b77a27d41e2eecb5463bfb5260221e2103b0c
Merlin <146963793+miftane@users.noreply.github.com>
2026-02-21 10:46:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Пункт Мир!")
+print("Hello world!")
+print("Hello", "World" + "!")
+first = int(input("Input first number:\n"))
+second = int(input("Input second number:\n"))
+third = int(input("Input third number\n"))
+avg = (first + second + third) / 3
+print(f"AVG:{avg}")
